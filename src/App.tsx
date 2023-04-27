2f34568dd0279c13a055928a6d66d1c594893719
Ash Bars <barsegyan@topsystems.ru>
2023-04-27 20:48:14 +0300

    refactor: -добавить тест useCallback и React.memo

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {DynamicNameWrapper, UserInfo} from './components';
 import './App.scss';
+import {ComponentUseCallback} from "./hooksLearnings/useCallback&useMemo/UserInfo";
 
 
 
 function App() {
-
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
+        <button onClick={(e) => setCount(c => ++c)}> plus </button>
+        <b>Count is: {count}</b>
         <DynamicNameWrapper/>
-        <UserInfo name={'random user'}/>
+        {/*<UserInfo name={'random user'}/>*/}
+        <ComponentUseCallback name={'usecallback'}/>
     </div>
   );
 }
--- src/hooksLearnings/useCallback&useMemo/UserInfo.tsx
+++ src/hooksLearnings/useCallback&useMemo/UserInfo.tsx
@@ -1,16 +1,26 @@
-import React from 'react';
-import {useUserStatus} from "../../customHooks";
-
-import './useCallback&useMemo.scss';
+import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import {DynamicNameWrapper} from "../../components";
 
 type TUserInfo = {
     name: string;
 }
+
+
 export function ComponentUseCallback(props: React.PropsWithChildren<TUserInfo>) {
+
+    const MemoizedFunction = useCallback(React.memo((props: any) => {
+        console.log('check without reactMemo')
+           return <>dataGrid</>
+    }), []);
+
+    useEffect(() => {
+
+    }, [MemoizedFunction])
 
     return (
         <div>
-
+            <h3>{props.name} test</h3>
+            <MemoizedFunction/>
         </div>
     );
 }
